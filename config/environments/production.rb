d7a3d33cdab1adf8fbeba1116d94315492dc8f3c
Obinna Ezebuiroh <ove201@nyu.edu>
2015-01-20 11:21:45 -0500

    Added feedback email to privacy and made config.force_ssl = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ CI::Application.configure do
 
 # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
 
 
